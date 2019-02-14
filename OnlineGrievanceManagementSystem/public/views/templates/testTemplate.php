8071f7da76d2e56b062e00db2981d45c3854fd28
Prateek Kapoor <prateekkapoor323@gmail.com>
2019-02-14 12:43:10 +0530

    Added templates folder

--- OnlineGrievanceManagementSystem/public/views/templates/testTemplate.php
+++ OnlineGrievanceManagementSystem/public/views/templates/testTemplate.php
@@ -1,9 +1,8 @@
-
 <head>
     <link rel="stylesheet" href="css/test.css">
     <title>Document</title>
 </head>
 <div>
 
-    <h2>Test Partials for Online Grievance Management System</h2>
+    <h2>Test Template for Online Grievance Management System</h2>
 </div>
