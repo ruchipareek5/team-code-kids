9ef4c9a239fb0cd0c7d1eac76fb724e4011997c0
shashanksameer <34919042+shashanksameer@users.noreply.github.com>
2019-02-18 15:53:51 +0530

    Author : Shashank
    Updates : 
    - Added favicon icon (supported in all types of browsers)
    - changes are made in public/index.php file
    
    File Upload : 
    favicon folder added in public/assets/favicon

--- OnlineGrievanceManagementSystem/public/views/index.php
+++ OnlineGrievanceManagementSystem/public/views/index.php
@@ -4,9 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Grievance System</title>
+    <!-- ****** favicons ****** -->
+    <link rel="shortcut icon" href="./../../assets/favicon/favicon.ico">
+    <link rel="icon" sizes="16x16 32x32 64x64" href="./../../assets/favicon/favicon.ico">
+    <link rel="icon" type="image/png" sizes="196x196" href="./../../assets/favicon/favicon-192.png">
+    <link rel="icon" type="image/png" sizes="160x160" href="./../../assets/favicon/favicon-160.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="./../../assets/favicon/favicon-96.png">
+    <link rel="icon" type="image/png" sizes="64x64" href="./../../assets/favicon/favicon-64.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./../../assets/favicon/favicon-32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./../../assets/favicon/favicon-16.png">
+    <link rel="apple-touch-icon" href="./../../assets/favicon/favicon-57.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="./../../assets/favicon/favicon-114.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="./../../assets/favicon/favicon-72.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="./../../assets/favicon/favicon-144.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="./../../assets/favicon/favicon-60.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="./../../assets/favicon/favicon-120.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="./../../assets/favicon/favicon-76.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="./../../assets/favicon/favicon-152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="./../../assets/favicon/favicon-180.png">
+    <meta name="msapplication-TileColor" content="#FFFFFF">
+    <meta name="msapplication-TileImage" content="./../../assets/favicon/favicon-144.png">
+    <meta name="msapplication-config" content="./../../assets/favicon/browserconfig.xml">
+    <!-- ****** favicons ****** -->
+    <title>Online Grievance Management System</title>
     <!-- jquery should be loaded first ele chart or ng view wont work -->
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
     integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
     crossorigin="anonymous"></script>
     <script src="./../../js/libraries/angular.min.js"></script>
