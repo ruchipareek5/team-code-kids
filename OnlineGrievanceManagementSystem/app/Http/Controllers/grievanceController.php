ac44d46b07fa81a874c25293239b76cb42c6238e
sachint985 <sacht985@gmail.com>
2019-02-20 19:57:30 +0530

    Search Application code added

--- OnlineGrievanceManagementSystem/app/Http/Controllers/grievanceController.php
+++ OnlineGrievanceManagementSystem/app/Http/Controllers/grievanceController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use App\Grievance;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class grievanceController extends Controller
 {
@@ -36,7 +38,11 @@ class grievanceController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $type = $request->get('type');
+        $description = $request->get('description');
+        $file = $request->get('file');
+        $student_id = DB::table('user_student')->where('email',Auth::user()->email)->get(['id','college_id']);
+        $department_id = DB::table('table_department')->where('type',$type)->get(['']);
       
 
     }
@@ -49,7 +55,21 @@ class grievanceController extends Controller
      */
     public function show($id)
     {
-        //
+        $email = Auth::user()->email;
+        $student_id = DB::table('user_student')->where('email', $email)->get(['id']);
+        $grievances = DB::table('table_grievance')->where('id',$id)->get(['id','type','created_at','documents','department_id']);
+        $grievance_status = DB::table('table_grievance_status')->where('grievance_id',$id)->get(['status','eta']);
+        $department_name = DB::table('table_department')->where('id',$grievances[0]->department_id)->get(['name']);
+        $data = [
+            'id' => $grievances[0]->id,
+            'type' => $grievances[0]->type,
+            'created_at'=> $grievances[0]->created_at,
+            'documents' => $grievances[0]->documents,
+            'committe_assigned' => $department_name[0]->name,
+            'status' => $grievance_status[0]->status,
+            'eta' => $grievance_status[0]->eta
+        ];
+        return json_encode($data);
     }
 
     /**
