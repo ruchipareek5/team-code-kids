cd489391b8fa8c8e7dae53786f013ef42b3904d9
sachint985 <sacht985@gmail.com>
2019-02-21 11:53:48 +0530

    GrievanceStatus file added

---
+++ OnlineGrievanceManagementSystem/app/GrievanceStatus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GrievanceStatus extends Model
+{
+    protected $table = 'table_grievance_status';
+    public $timestamps = false;
+}
