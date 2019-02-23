6f4768f457b625b69c03392d9ac19521852830e4
kaushikojha1997 <kaushikojha1997@gmail.com>
2019-02-23 13:45:06 +0530

    Added Committee Service

---
+++ OnlineGrievanceManagementSystem/public/js/services/committeeService.js
@@ -0,0 +1,35 @@
+var grievancesystem=angular.module('grievancesystem');
+
+grievancesystem.service('committeeService',function ($http,API_URL) {
+	
+
+
+	this.getGrievance=function () {
+		var url = API_URL + 'aicte/grievances';
+		return $http.get(url);
+
+		}
+		
+	this.searchGrievance=function(t,d) {
+		var url = API_URL + 'aicte/grievanceSearch';
+		return $http({
+		    'url': url, 
+		    method: "GET",
+		    params: {
+		    	'type': t,
+		    	'data':d
+		    }
+		 	});
+		}
+
+	this.institute_search=function(institute_id){
+		var url =API_URL + 'grievance/aicte/importantinfo/institute/'+ institute_id;
+		return $http.get(url);
+	}
+
+	this.university_search=function(university_id){
+		var url =API_URL + 'grievance/aicte/importantinfo/university/'+ university_id;
+		return $http.get(url);
+	}
+	
+});
