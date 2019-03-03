c7af801d1d60b3ef94061b0dfbd9ac27a541a8ee
sachint985 <sacht985@gmail.com>
2019-03-03 13:45:30 +0530

    HTML File uploaded for the Registration email

---
+++ OnlineGrievanceManagementSystem/public/views/email.blade.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <style type="text/css">
+
+        #p1{
+            font-size: 2vw;
+
+        }
+        #p2{
+            font-size: 10px;
+        }
+
+    </style>
+</head>
+<body>
+<p id="p1" class="pp">Welcome to AICTE grievance management system</p>
+<p>Congratulations, you are now registered on AICTE grievance management system </p>
+<p>Your UserId: {{$username}}</p>
+<p>Your Password: {{$password}}</p>
+<p><bold>Note: </bold>Please don't share this email with anyone. By doing the such, Your accound can be exposed to the other person</p>
+<p>Thankyou</p>
+<p id="p2">AICTE grams</p>
+</body>
+</html>
+
